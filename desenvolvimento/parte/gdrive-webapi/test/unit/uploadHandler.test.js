4d06ca238ebaf292cf65a86bc27e70fea30fe4af
Matpierro <matpierro570@gmail.com>
2021-09-18 16:25:17 -0300

    Criando pastas e arquivos para trabalhar com testes unitario e trabalhar com streams em node

--- desenvolvimento/parte/gdrive-webapi/test/unit/uploadHandler.test.js
+++ desenvolvimento/parte/gdrive-webapi/test/unit/uploadHandler.test.js
@@ -4,6 +4,7 @@ import {
     expect,
     jest
 } from '@jest/globals'
+import UploadHandler from '../../src/uploadHandler.js'
 import Routes from './../../src/routes.js'
 
 describe('#UploadHandler test suite', () => {
@@ -13,7 +14,24 @@ describe('#UploadHandler test suite', () => {
     }
 
     describe('#registerEvents', () => {
-        test.todo('should call onFile and onFinish functions on Busboy instance', () => {
+        test('should call onFile and onFinish functions on Busboy instance', () => {
+            const uploadHandler = new UploadHandler({
+                io: ioObj,
+                socketId: '01'
+            })
+
+            jest.spyOn(uploadHandler, uploadHandler.onFile.name)
+                .mockResolvedValue()
+
+            const headers = {
+                'content-type': 'multipart/form-data; boundary='
+            }
+            const fn = jest.fn()
+            uploadHandler.registerEvents(headers, fn)
+
+            expect(uploadHandler.onFile).toHaveBeenCalled()
+
+            expect(fn).toHaveBeenCalled()
 
         })
 
