3a022976cf561970b8f8efab5a06ddaaf041cb04
Matpierro <matpierro570@gmail.com>
2021-09-18 16:32:04 -0300

    criando gerador de Readable Streams

--- desenvolvimento/parte/gdrive-webapi/test/_util/testUtil.js
+++ desenvolvimento/parte/gdrive-webapi/test/_util/testUtil.js
@@ -1,3 +1,15 @@
+import { Readable } from 'stream'
 export default class TestUtil {
-    static
+
+    static generateReadableStream(data) {
+        return new Readable({
+            async read() {
+                for (const item of data) {
+                    this.push(item)
+                }
+
+                this.push(null)
+            }
+        })
+    }
 }
