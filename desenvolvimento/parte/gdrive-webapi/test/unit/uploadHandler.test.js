fca2a0497312c4f2115456e2417f05cfa3495746
Matpierro <matpierro570@gmail.com>
2021-09-20 20:09:51 -0300

    Finalizado testes para caso seja falso o tratamento de arquivos

--- desenvolvimento/parte/gdrive-webapi/test/unit/uploadHandler.test.js
+++ desenvolvimento/parte/gdrive-webapi/test/unit/uploadHandler.test.js
@@ -120,11 +120,62 @@ describe('#UploadHandler test suite', () => {
             expect(onWrite.mock.calls.join()).toEqual(messages.join())
 
         })
+
+        test('given message timerDelay as 2secs it should emit only two messages during 2 seconds period', async() => {
+            jest.spyOn(ioObj, ioObj.emit.name)
+
+            const day = '2021-07-01 01:01'
+
+            const twoSecondsPeriod = 2000
+
+            const onFirstLastMessageSent = TestUtil.getTimeFromDate(`${day}:00`)
+
+            const onFirstCanExecute = TestUtil.getTimeFromDate(`${day}:02`)
+            const onSecondUpdateLastMessageSent = onFirstCanExecute
+
+            const onSecondCanExecute = TestUtil.getTimeFromDate(`${day}:03`)
+
+            const onThirdCanExecute = TestUtil.getTimeFromDate(`${day}:04`)
+
+
+            TestUtil.mockDateNow(
+                [
+                    onFirstLastMessageSent,
+                    onFirstCanExecute,
+                    onSecondUpdateLastMessageSent,
+                    onSecondCanExecute,
+                    onThirdCanExecute
+                ]
+            )
+
+            const messages = ["hello", "hello", "world"]
+            const filename = 'filename.avi'
+            const expectedMessageSent = 2
+
+
+            const source = TestUtil.generateReadableStream(messages)
+            const handler = new UploadHandler({
+                messageTimeDelay: twoSecondsPeriod,
+                io: ioObj,
+                socketId: '01'
+            })
+
+            await pipeline(
+                source,
+                handler.handleFileBytes(filename)
+            )
+
+            expect(ioObj.emit).toHaveBeenCalledTimes(expectedMessageSent)
+
+            const [firstCallResult, secondCallResult] = ioObj.emit.mock.calls
+
+            expect(firstCallResult).toEqual([handler.ON_UPLOAD_EVENT, { processedAlredy: "hello".length, filename }])
+            expect(secondCallResult).toEqual([handler.ON_UPLOAD_EVENT, { processedAlredy: messages.join("").length, filename }])
+
+        })
     })
 
     describe('#canExecute', () => {
-
-
         test('should return true when time is later than specified delay', () => {
             const timerDelay = 1000
             const uploadHandler = new UploadHandler({
@@ -133,12 +184,10 @@ describe('#UploadHandler test suite', () => {
                 messageTimeDelay: timerDelay
             })
 
-            const tickNow = TestUtil.getTimeFromDate('2021-07-01 00:03')
+            const tickNow = TestUtil.getTimeFromDate('2021-07-01 00:00:03')
             TestUtil.mockDateNow([tickNow])
 
-            const tickThreeSecondsBefore = TestUtil.getTimeFromDate('2021-07-01 00:00')
-            const lastExecution = tickThreeSecondsBefore
-
+            const lastExecution = TestUtil.getTimeFromDate('2021-07-01 00:00:00')
 
             const result = uploadHandler.canExecute(lastExecution)
             expect(result).toBeTruthy()
@@ -147,22 +196,20 @@ describe('#UploadHandler test suite', () => {
 
         test('should return false when time isnt later than specified delay', () => {
 
-            const timerDelay = 1000
+            const timerDelay = 3000
             const uploadHandler = new UploadHandler({
                 io: {},
                 socketId: '',
                 messageTimeDelay: timerDelay
             })
 
-            const tickNow = TestUtil.getTimeFromDate('2021-07-01 00:03')
-            TestUtil.mockDateNow([tickNow])
+            const now = TestUtil.getTimeFromDate('2021-07-01 00:00:02')
+            TestUtil.mockDateNow([now])
 
-            const tickThreeSecondsBefore = TestUtil.getTimeFromDate('2021-07-01 00:00')
-            const lastExecution = tickThreeSecondsBefore
+            const lastExecution = TestUtil.getTimeFromDate('2021-07-01 00:00:01')
 
-
-            const result = uploadHandler.canExecute(lastExecution)
-            expect(result).toBeTruthy()
+            const result = uploadHandler.canExecute(now)
+            expect(result).toBeFalsy()
 
         })
 
