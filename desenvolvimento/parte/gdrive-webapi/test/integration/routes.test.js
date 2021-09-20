bdbbd616e0227bc5b7aeb22f1c69843882f77090
Matpierro <matpierro570@gmail.com>
2021-09-20 20:21:56 -0300

    Ambiente de teste de integração

---
+++ desenvolvimento/parte/gdrive-webapi/test/integration/routes.test.js
@@ -0,0 +1,18 @@
+import {
+    describe,
+    test,
+    expect,
+    jest
+} from '@jest/globals'
+import fs from 'fs'
+import FileHelper from '../../src/fileHelper.js'
+import Routes from './../../src/routes.js'
+
+describe('#FileHelper', () => {
+
+    describe('#getFileStatus', () => {
+        test('it should return files statuses in correct format', async() => {
+
+        })
+    })
+})
