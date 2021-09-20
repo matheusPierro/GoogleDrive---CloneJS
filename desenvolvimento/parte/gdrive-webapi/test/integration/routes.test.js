372679817927eda8d4673428f2f1cc70e9f45b76
Matpierro <matpierro570@gmail.com>
2021-09-20 20:49:01 -0300

    Trabalhando com o form data no mock

--- desenvolvimento/parte/gdrive-webapi/test/integration/routes.test.js
+++ desenvolvimento/parte/gdrive-webapi/test/integration/routes.test.js
@@ -6,26 +6,33 @@ import {
 } from '@jest/globals'
 import fs from 'fs'
 import FileHelper from '../../src/fileHelper.js'
+
 import Routes from './../../src/routes.js'
+import FormData from 'form-data'
+import TestUtil from '../_util/testUtil.js'
 
 describe('#Routes Integration Test', () => {
 
     describe('#getFileStatus', () => {
         test('should upload file to the folder', async() => {
             const filename = 'miranha.jpg'
+            const fileStream = fs.createReadStream(`./test/integration/mocks/${filename}`)
+            const response = TestUtil.generateWritableStream(() => {})
+
+            const form = new FormData()
+            form.append('photo', fileStream)
+
             const defaultParams = {
-                request: {
-                    headers: {
-                        'Content-Type': 'multipart/form-data'
-                    },
-                    method: '',
-                    body: {}
-                },
-                response: {
+                request: Object.assign(form, {
+                    headers: form.getHeaders(),
+                    method: 'POST',
+                    url: '?socketId=10'
+                }),
+                response: Object.assign(response, {
                     setHeader: jest.fn(),
                     writeHead: jest.fn(),
                     end: jest.fn()
-                },
+                }),
                 values: () => Object.values(defaultParams)
             }
         })
