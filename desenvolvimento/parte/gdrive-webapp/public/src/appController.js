8795b41b88111c13e111561dbeb0f3187e6bb2d1
Matpierro <matpierro570@gmail.com>
2021-09-26 21:38:07 -0300

    ainda tentando consertar erro pra dar push

---
+++ desenvolvimento/parte/gdrive-webapp/public/src/appController.js
@@ -0,0 +1,66 @@
+export default class AppController {
+    constructor({ connectionManager, viewManager }) {
+        this.connectionManager = connectionManager
+        this.viewManager = viewManager
+
+        this.uploadingFiles = new Map()
+    }
+
+    async initialize() {
+        this.viewManager.configureFileBtnClick()
+        this.viewManager.configureModal()
+        this.viewManager.configureOnFileChange(this.onFileChange.bind(this))
+        this.connectionManager.configureEvents({
+            onProgress: this.onProgress.bind(this)
+        })
+
+        this.viewManager.updateStatus(0);
+        await this.updateCurrentFiles()
+    }
+
+    async onProgress({ processedAlredy, filename }) {
+        console.debug({ processedAlredy, filename })
+
+        const file = this.uploadingFiles.get(filename)
+        const alredyProcessed = Math.ceil(processedAlredy / file.size * 100)
+        this.updateProgress(file, alredyProcessed)
+
+        if (alredyProcessed < 98) return;
+
+        return this.updateCurrentFiles()
+    }
+
+    updateProgress(file, percent) {
+        const uploadingFiles = this.uploadingFiles
+        file.percent = percent
+
+        const total = [...uploadingFiles.values()]
+            .map(({ percent }) => percent ?? 0)
+            .reduce((total, current) => total + current, 0)
+
+        this.viewManager.updateStatus(total)
+    }
+
+    async onFileChange(files) {
+        this.viewManager.openModal()
+        this.viewManager.updateStatus(0)
+
+        const requests = []
+        for (const file of files) {
+            this.uploadingFiles.set(file.name, file)
+            requests.push(this.connectionManager.uploadFile(file))
+        }
+
+        await Promise.all(requests)
+        this.viewManager.updateStatus(100)
+
+        setTimeout(() => this.viewManager.closeModal(), 1000);
+
+        await this.updateCurrentFiles()
+    }
+
+    async updateCurrentFiles() {
+        const files = await this.connectionManager.currentFiles()
+        this.viewManager.updateCurrentFiles(files)
+    }
+}
